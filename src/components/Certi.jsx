cf03a1daa9fca20127397243ff06c1347abe209b
ChanasakB <c.auetidsakul@gmail.com>
2024-03-19 23:06:50 +0700

    [ADD] init project

---
+++ src/components/Certi.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from "@/styles/Certi.module.css"
+
+export const Certi = () => {
+  return (
+    <>
+    <div className={styles.container}>
+      {/* <div className={styles.img}>img</div> */}
+      <div>
+        <Image src={"/certificate.png"} width={1500} height={800} layout="responsive" alt='logo1'/>
+      </div>
+      <div className={styles.right}>
+        <div>
+          <h1>professional certification in teaching</h1>
+        </div>
+        <div className={styles.buttonbtn}>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, est iure! Laudantium quisquam voluptates voluptatibus maiores, voluptatem nemo ad quidem perferendis. Harum cum illo minus repellat sit enim, quia explicabo, ipsum eius laboriosam ea, nihil eaque alias atque qui reprehenderit amet. Sapiente ut rem blanditiis quam. Delectus dolores consequuntur minima.</p>
+            <button>see more</button>
+            </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Certi;
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "../components/Head";
+import Section from "../components/Section";
+import LiveEvent from "../components/LiveEvent";
+import UpEvent from "../components/UpEvent";
+import Certi from "../components/Certi";
+
+export default function Home() {
+  return (
+    <>
+     <div>
+      <Head />
+      <Section />
+      <LiveEvent />
+      <UpEvent />
+      <Certi />
+     </div>
+     
+    </>
+  );
+}
